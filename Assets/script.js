01c56701521689eb004822df28606bc833bacbf7
Melody Monarch <oliva.sam.so@gmail.com>
2020-11-21 16:27:32 -0600

    code cleaned up a bit and added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,32 @@
-//console.log("hi")
+// When initial page loads up, hiding the hard code in the HTMl
 $("#weatherInfoArea").attr("style", "display : none")
+
+// display all of the past cities the user has searched
 displayLocalStorage()
+
+// when the user has nothing in local storage, nothing should load on page
 if (localStorage.getItem("weatherAPI") !== null) {
     viewLastCity();
 }
 
+// when the magnifying glass button is click, weather data should show up
 $("#searchBtn").on("click", function (event) {
     event.preventDefault()
     let userCity = $("#userCity").val()
-    // console.log(userCity)
     lookupCity(userCity)
     fiveDayForecast(userCity)
     var currentDate = moment().format("l")
-    console.log(currentDate)
     $("#currentCityName").text(`${userCity} ${currentDate}`)
     $("#weatherInfoArea").attr("style", "display : block")
 })
 
+// Current weather of current day of city user entered
 function lookupCity(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=aa4d71cf73d20b64aa3e608785427cd8&units=imperial`
     $.ajax({
         method: "GET",
         url: queryURL
     }).then(function (apiData) {
-        //  console.log(apiData)
         $("#temperature").text(`Temperature: ${apiData.main.temp} °F`)
         $("#humidity").text(`Humidity: ${apiData.main.humidity}%`)
         $("#windSpeed").text(`Windspeed: ${apiData.wind.speed} MPH`)
@@ -32,7 +35,6 @@ function lookupCity(city) {
         var lon = apiData.coord.lon
         lookupUV(lat, lon)
         var previousCity = JSON.parse(localStorage.getItem("weatherAPI")) || []
-        // console.log(previousCity)
         if (previousCity.indexOf(city) === -1) {
             previousCity.unshift(city)
             localStorage.setItem("weatherAPI", JSON.stringify(previousCity))
@@ -42,13 +44,13 @@ function lookupCity(city) {
 
 }
 
+// UV numbers and color coding for low, moderate, high, and very high
 function lookupUV(lat, lon) {
     var queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=aa4d71cf73d20b64aa3e608785427cd8&units=imperial`
     $.ajax({
         method: "GET",
         url: queryURL
     }).then(function (apiData) {
-        console.log(apiData.value)
         if (0 <= apiData.value && apiData.value <= 2) {
             $("#uvIndex").text(`${apiData.value}`).attr("style", "background-color:green")
         } else if (2 < apiData.value && apiData.value <= 5) {
@@ -63,6 +65,7 @@ function lookupUV(lat, lon) {
     })
 }
 
+// previous cities search will be display as button and if they click on buttons, the weather info will pop up
 function displayLocalStorage() {
     var previousCities = JSON.parse(localStorage.getItem("weatherAPI")) || []
     var allCities = ""
@@ -72,13 +75,10 @@ function displayLocalStorage() {
     $("#previousCitiesList").html(allCities)
 
     $(".previousCity").on("click", function () {
-        console.log("hi")
-        //   console.log(this.textContent)
         var localStorageCity = this.textContent
         lookupStorageCity(localStorageCity)
         fiveDayForecast(localStorageCity)
         var currentDate = moment().format("l")
-        //console.log(currentDate)
         $("#currentCityName").text(localStorageCity + (" (" + currentDate + ")"))
         $("#weatherInfoArea").attr("style", "display : block")
     })
@@ -101,15 +101,14 @@ function displayLocalStorage() {
     }
 }
 
+// Weather for next 5 days of current city searched
 function fiveDayForecast(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=aa4d71cf73d20b64aa3e608785427cd8&units=imperial`
     $.ajax({
         method: "GET",
         url: queryURL
     }).then(function (apiData) {
-        //       console.log(apiData)
         for (var i = 1; i < 6; i++) {
-            //   console.log(apiData.list[i].weather[0].icon)
             var icon = apiData.list[i].weather[0].icon
             var tomorrowDate = moment().add(i, "days").format("l")
             $("#" + i + "-date").text(tomorrowDate)
@@ -120,6 +119,7 @@ function fiveDayForecast(city) {
     })
 }
 
+// Once local storage isn't empty, the next time the users page load it will display the last city they searched in the search bar (not button clicked)
 function viewLastCity() {
     var pastCity = JSON.parse(localStorage.getItem("weatherAPI"))
     var currentDate = moment().format("l")
